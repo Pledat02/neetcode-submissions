548b2359fed3fb4bc32658bdb2bdb2820cd21574
phamledat <95115578+Pledat02@users.noreply.github.com>
2026-08-28 22:17:30 +0700

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+      String str = "";
+        for(String s : strs ){
+            str += s.length()+"#"+s;
+        }
+        return str;
+    }
+
+    public List<String> decode(String str) {
+        List<String> rs = new ArrayList();
+           for(int i = 0; i < str.length(); ){
+            int slashIndex = str.indexOf('#', i);
+            int len = Integer.parseInt(str.substring(i, slashIndex));
+            i = slashIndex + 1;
+            rs.add(str.substring(i, i + len));
+            i += len;
+        }
+         return rs;
+    }
+}
