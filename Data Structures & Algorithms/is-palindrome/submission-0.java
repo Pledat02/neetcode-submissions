5028e3b653426a5c39435629e47ff237d3103c56
phamledat <95115578+Pledat02@users.noreply.github.com>
2026-09-07 14:34:37 +0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String raw = s.trim().toLowerCase() ;
+        char[] cArr = raw.toCharArray();
+        Deque<Character> linkedList = new LinkedList();
+        for (int i = 0 ; i<cArr.length;i++){
+            if(Character.isLetter(cArr[i]) || Character.isDigit(cArr[i])){
+                linkedList.add(cArr[i]);
+            }
+        }
+        while(linkedList.size()>1){
+            System.out.println(linkedList);
+            if(linkedList.pollFirst()!=linkedList.pollLast()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
